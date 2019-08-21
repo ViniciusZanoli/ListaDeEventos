a04539fe3a2a22dce4c4755d790c26bab5913794
Vinicius Zanoli <viniciusdasilva.922@gmail.com>
2019-08-21 19:05:54 -0300

    Inicio do projeto

---
+++ src/main/java/com/vinicius/ListaDeEventos/ListaDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.ListaDeEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeEventosApplication.class, args);
+	}
+
+}
